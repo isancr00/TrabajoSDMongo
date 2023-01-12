d7742671267129c85bcafb9c0ceee5d15b196532
Iván Sánchez <isancr00@estudiantes.unileon.es>
2023-01-12 17:28:28 +0100

    Elimino base

--- proyecto/src/index.js
+++ proyecto/src/index.js
@@ -1,5 +1,25 @@
 const app = require('./app');
+const mongoose = require('mongoose')
 
+mongoose.connect('mongodb://root:rootpassword@mongodb:27017/', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+
+    }).then(db => console.log('conexion exitosa'))
+    .catch(err => console.log('error: ', err))
+
+const Persona = mongoose.model('Persona',{ nombre: String,apellidos:String, edad:Number,genero:String,ocupacion:String})
+
+function insertarPersona(nombre,apellidos,edad,genero,ocupacion){
+    var persona = new Persona({nombre:nombre,apellidos:apellidos,edad:edad,genero:genero,ocupacion:ocupacion})
+
+    persona.save().then(() => console.log('Persona creada'))
+}
+
+ function verPersonas(){
+    const result =  Persona.find();
+    console.log('Las personas registradas son: ' + result)
+}
 
 async function main(){
     await app.listen(3000);
@@ -7,3 +27,12 @@ async function main(){
 }
 
 main();
+
+insertarPersona('Marco','Diaz Gonzalez',23,'Male','Working');
+insertarPersona('Lucas','Gómez Laso',45,'Male','Working');
+insertarPersona('Gala','Alonso Gonzalez',16,'Female','Studying');
+insertarPersona('Pablo','Diaz Gonzalez',18,'Male','Studying');
+
+verPersonas();
+
+
